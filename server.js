23142a483380232196311f1bfcb6e7da27209719
Tom-Dln <tom.delaunay.perso@gmail.com>
2024-05-17 15:35:25 +0200

    v1.0.1

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// Middleware pour analyser les requêtes JSON
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 
-// Base de données en mémoire pour les utilisateurs et leurs positions
 let users = [];
 
-// API RESTful pour gérer les utilisateurs et leurs positions
 app.post('/user', (req, res) => {
     const { id, name, position } = req.body;
     const user = { id, name, position };
@@ -24,16 +24,13 @@ app.get('/positions', (req, res) => {
     res.status(200).json(users);
 });
 
-// WebSocket pour la mise à jour en temps réel
 wss.on('connection', ws => {
     console.log('Nouvelle connexion WebSocket');
     ws.on('message', message => {
-        console.log('Message reçu:', message);
         const { id, position } = JSON.parse(message);
         const user = users.find(user => user.id === id);
         if (user) {
             user.position = position;
-            // Diffusez la mise à jour de la position à tous les clients connectés
             wss.clients.forEach(client => {
                 if (client.readyState === WebSocket.OPEN) {
                     client.send(JSON.stringify({ id, position }));
@@ -43,7 +40,6 @@ wss.on('connection', ws => {
     });
 });
 
-// Démarrage du serveur
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
